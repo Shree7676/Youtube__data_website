5648fd161062e55e1e044a171b7ce5a377025a57
Shree7676 <shreekantnandiyawar@gmail.com>
2023-04-12 18:57:17 +0530

    major part of yt project done

--- yt_project/data_files/routes.py
+++ yt_project/data_files/routes.py
@@ -1,12 +1,22 @@
 from data_files import app
 from flask import request,render_template
+from data_files import models
+
+DF_COLLECTION = []
+MERGED_DF = []
 
 @app.route('/')
 def home():
     return render_template('index.html')
 
-@app.route('/yt_decode', methods=['GET'])
+
+@app.route('/yt_decode', methods=["GET", "POST"])
 def yt_decode():
-    channel_name = request.args.get('search')
-    print(channel_name)
-    return "hi u did it"
+    global DF_COLLECTION, MERGED_DF
+
+    if request.method == "POST":
+        data = request.form['content']
+        list_data = data.split(',')
+        df, DF_COLLECTION, MERGED_DF = models.get_yt_df(list_data)
+        df.drop('playlist', inplace=True, axis=1)
+    return render_template('result.html', tables=[df.to_html(classes='table-info table table-hover align-middle', header="true")], titles=df.columns.values)
